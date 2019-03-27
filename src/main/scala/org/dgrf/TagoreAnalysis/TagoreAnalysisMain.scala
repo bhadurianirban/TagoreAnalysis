03fb8bfd6a00410a52e6a15e07e4f36361686421
bhadurianirban <bhaduri.anirban@gmail.com>
2019-03-27 10:28:17 +0530

    bari

--- src/main/scala/org/dgrf/TagoreAnalysis/TagoreAnalysisMain.scala
+++ src/main/scala/org/dgrf/TagoreAnalysis/TagoreAnalysisMain.scala
@@ -2,17 +2,24 @@ package org.dgrf.TagoreAnalysis
 
 import java.io.File
 
+import org.apache.hadoop.fs.{FileSystem, FileUtil,Path}
 import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
+
+
 object TagoreAnalysisMain {
+  var inputDir = "/home/bhaduri/MEGA/ML/inputdata"
+  var tempDir:String = _
+  var outPutDir:String = _
   def main(args: Array[String]): Unit = {
     println("Gheu")
     val sparkSession = SparkSession.builder().appName("bheua").master("local").getOrCreate()
 
     //val sqlContext = sparkSession.sqlContext
   //ss
-    val inputDir = "/home/dgrfi/MEGA/ML"
+    val d = new File(inputDir)
+    tempDir = d.getParent+"/temp/"
 
 
     val inputFileList = getListOfFiles(inputDir,".txt")
@@ -35,19 +42,22 @@ object TagoreAnalysisMain {
 
   def performAnalysis (f:File,sparkSession:SparkSession): Unit = {
     val fileName = f.getName.replaceAll(".txt","")
-    val outputFileDirectory = f.getParent+"/output/"
-    val outCountFileName = outputFileDirectory+fileName+"Count"
-    val outSentenceFileName = outputFileDirectory+fileName+"Sentence"
-    //println(outCountFileName+" "+outSentenceFileName+" "+outputFileDirectory)
-    val ta = new TextAnalyser(sparkSession,f.getAbsolutePath)
+
+    val outCountFileName = tempDir+fileName+"Count"
+    val outSentenceFileName = tempDir+fileName+"Sentence"
+    println(outCountFileName+" "+outSentenceFileName)
+    /*val ta = new TextAnalyser(sparkSession,f.getAbsolutePath)
     ta.countWords(outCountFileName)
     ta.countWordsPerLine(outSentenceFileName)
+    mergePartFiles(outCountFileName,sparkSession)*/
 
-    val wordCountFileList = getListOfFiles(outCountFileName,".csv")
-    val sentenceFileList = getListOfFiles(outSentenceFileName,".csv")
-    wordCountFileList.foreach(println)
-    sentenceFileList.foreach(println)
 
+  }
+  def mergePartFiles(outCountFileName:String,sparkSession:SparkSession): Unit = {
+    //use hadoop FileUtil to merge all partition csv files into a single file
+
+    val fs = FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)
+    FileUtil.copyMerge(fs, new Path(outCountFileName), fs, new Path("outout/"+outCountFileName+"target.csv"), true, sparkSession.sparkContext.hadoopConfiguration, null)
   }
   
   
