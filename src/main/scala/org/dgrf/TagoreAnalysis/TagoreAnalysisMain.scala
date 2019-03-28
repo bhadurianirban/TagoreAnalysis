6b24a35af3a0d3e503edda0b16812db3167affd6
bhadurianirban <bhaduri.anirban@gmail.com>
2019-03-28 13:11:43 +0530

    bari

--- src/main/scala/org/dgrf/TagoreAnalysis/TagoreAnalysisMain.scala
+++ src/main/scala/org/dgrf/TagoreAnalysis/TagoreAnalysisMain.scala
@@ -1,10 +1,12 @@
 package org.dgrf.TagoreAnalysis
 
-import java.io.File
+import java.io.{BufferedWriter, File, FileWriter}
 
-import org.apache.hadoop.fs.{FileSystem, FileUtil,Path}
+import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+import scala.io.Source
 
 
 
@@ -20,15 +22,12 @@ object TagoreAnalysisMain {
   //ss
     val d = new File(inputDir)
     tempDir = d.getParent+"/temp/"
+    outPutDir = d.getParent+"/output/"
 
+    /*val inputFileList = getListOfFiles(inputDir,".txt")
+    inputFileList.foreach(x=> performAnalysis(x.getName,sparkSession))*/
 
-    val inputFileList = getListOfFiles(inputDir,".txt")
-    inputFileList.foreach(x=> performAnalysis(x,sparkSession))
-
-    //val outputFileName = "/home/bhaduri/MEGA/ML/DurgeshnandiniSentence"
-
-    /*val ta = new TextAnalyser(sparkSession,inputFileName,outputFileName)
-    ta.countWords()*/
+    splitWCandCC()
   }
 
   def getListOfFiles(dir: String,extension:String):List[File] = {
@@ -40,26 +39,58 @@ object TagoreAnalysisMain {
     }
   }
 
-  def performAnalysis (f:File,sparkSession:SparkSession): Unit = {
-    val fileName = f.getName.replaceAll(".txt","")
+  def performAnalysis (inputFileName:String,sparkSession:SparkSession): Unit = {
+    val fileNameWithoutExtension = inputFileName.replaceAll(".txt","")
 
-    val outCountFileName = tempDir+fileName+"Count"
-    val outSentenceFileName = tempDir+fileName+"Sentence"
-    println(outCountFileName+" "+outSentenceFileName)
-    /*val ta = new TextAnalyser(sparkSession,f.getAbsolutePath)
-    ta.countWords(outCountFileName)
-    ta.countWordsPerLine(outSentenceFileName)
-    mergePartFiles(outCountFileName,sparkSession)*/
+    val outTempFilePath = tempDir+fileNameWithoutExtension
+
+    val inputFileWithFullPath = inputDir+File.separator+inputFileName
+    //println(outCountFileName+" "+outSentenceFileName+" "+inputFileWithFullPath)
+    val ta = new TextAnalyser(sparkSession,inputFileWithFullPath)
+
+    ta.countWords(outTempFilePath)
+
+    mergePartFiles(fileNameWithoutExtension,sparkSession,"WordCount")
+    ta.countWordsPerLine(outTempFilePath)
+
+    mergePartFiles(fileNameWithoutExtension,sparkSession,"Sentence")
 
 
   }
-  def mergePartFiles(outCountFileName:String,sparkSession:SparkSession): Unit = {
+  def mergePartFiles(fileNameWithoutExtension:String,sparkSession:SparkSession,resultFunction:String): Unit = {
     //use hadoop FileUtil to merge all partition csv files into a single file
+    val outTempFilePath = tempDir+fileNameWithoutExtension
 
     val fs = FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)
-    FileUtil.copyMerge(fs, new Path(outCountFileName), fs, new Path("outout/"+outCountFileName+"target.csv"), true, sparkSession.sparkContext.hadoopConfiguration, null)
+    val resultFilePath = outPutDir+resultFunction+File.separator+fileNameWithoutExtension+".csv"
+    val dstPath = new Path(resultFilePath)
+    if (fs.exists(dstPath)) {
+      fs.delete(dstPath,true)
+    }
+
+    FileUtil.copyMerge(fs, new Path(outTempFilePath), fs, dstPath, true, sparkSession.sparkContext.hadoopConfiguration, null)
   }
   
-  
+  def splitWCandCC(): Unit = {
+    val sentenceDir = outPutDir+File.separator+"Sentence"
+    val inputFileList = getListOfFiles(sentenceDir,".csv")
+    inputFileList.foreach(file=>writeWC(file))
 
+  }
+
+  def writeWC(sentenceFile:File): Unit = {
+    val bufferedSource = Source.fromFile(sentenceFile)
+    val outFileWriter = new File("CC")
+    val bw = new BufferedWriter(new FileWriter(outFileWriter))
+
+    for (line <- bufferedSource.getLines) {
+      val XYData = line.split(",")
+
+      //println (lineCounter+" n "+gheu(0)+" g "+gheu(1))
+      bw.write(XYData(0)+"\n")
+
+    }
+    bw.close()
+    bufferedSource.close()
+  }
 }
